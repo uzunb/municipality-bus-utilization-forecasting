375f5e7607e059479b089d03e16a25c23a914bea
uzunb <batuuzun08@gmail.com>
2023-04-12 07:12:30 +0300

    Add ForecastingModel abstract class for streamlit deployment

---
+++ ForecastingModel.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import pathlib
+from abc import ABC, abstractmethod
+
+
+PROJECT_DIR = pathlib.Path.cwd()
+DATA_DIR = PROJECT_DIR / "data"
+
+
+class ForecastingModel(ABC):
+    def __init__(self, model, modelName, modelDescription):
+        self.model = model
+        self.modelName = modelName
+        self.modelDescription = modelDescription
+
+    @abstractmethod
+    def fit(self, municipalityId):
+        pass
+
+    @abstractmethod
+    def predict(self, municipalityId):
+        pass
+
+    @abstractmethod
+    def plot(self, municipalityId):
+        pass
+
+    @abstractmethod
+    def forecast(self, municipalityId):
+        pass
+
+    def intro(self):
+        st.markdown("## {}".format(self.modelName))
+        st.markdown(self.modelDescription)
+
+    def getModel(self):
+        return self.model
+
+    def getModelName(self):
+        return self.modelName
+
+    def getModelDescription(self):
+        return self.modelDescription
